f91ad8d9b040b3d8e4e3c66a83fb114418e58f57
Lokeshvarma2620 <Lokeshvarma1014@gmail.com>
2023-04-05 16:15:05 -0500

    updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kiteRouter = require('./routes/kite');
 var boardRouter = require('./routes/board');
-var SelectorRouter = require('./routes/selector');s
+var SelectorRouter = require('./routes/selector');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
