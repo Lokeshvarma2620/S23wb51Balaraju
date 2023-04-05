3dd5db03a2ff2842e41fc10bf35688d63f63f18c
Lokeshvarma2620 <Lokeshvarma1014@gmail.com>
2023-04-05 16:03:47 -0500

    updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var carRouter = require('./routes/car');
+var kiteRouter = require('./routes/kite');
 var boardRouter = require('./routes/board');
 var SelectorRouter = require('./routes/selector');s
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/kite', carRouter);
+app.use('/kite', kiteRouter);
 app.use('/board', boardRouter);
 app.use('/Selector', SelectorRouter);
 
