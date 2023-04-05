f9738fb89690742d7231951dc3804b126d1ad49e
Lokeshvarma2620 <Lokeshvarma1014@gmail.com>
2023-04-05 18:07:30 -0500

    ok

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kiteRouter = require('./routes/kite');
 var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kite', kiteRouter);
 app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
+
 
 
 
