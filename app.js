51392f8fa1610c7414465d888418ccda6d614364
Lokeshvarma2620 <Lokeshvarma1014@gmail.com>
2023-04-05 17:44:25 -0500

    updated

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var kiteRouter = require('./routes/kite');
 var boardRouter = require('./routes/board');
-var SelectorRouter = require('./routes/selector');
+
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +26,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/kite', kiteRouter);
 app.use('/board', boardRouter);
-app.use('/Selector', SelectorRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
