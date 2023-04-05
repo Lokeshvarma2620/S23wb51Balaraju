c26e97e7639229412bba7253c6d45ef0772abed4
Lokeshvarma2620 <Lokeshvarma1014@gmail.com>
2023-04-05 16:00:15 -0500

    updated

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
+var carRouter = require('./routes/car');
+var boardRouter = require('./routes/board');
+var SelectorRouter = require('./routes/selector');s
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/kite', carRouter);
+app.use('/board', boardRouter);
+app.use('/Selector', SelectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
